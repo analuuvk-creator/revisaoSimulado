734062c6157b40450be679cf16e5aea72bc30f06
Analuuvk-creator <analuuvk@gmail.com>
2026-05-07 09:02:22 -0300

    implementado a seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Produto;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +17,31 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@senai.com',
+            'password' => Hash::make('123')
+        ]);
+
+        Produto::create([
+            'name' => 'Martelo',
+            'valor' => 50.00,
+            'qtd_estoque' => 100,
+            'qtd_minima' => 10
+        ]);
+
+        Produto::create([
+            'name' => 'Chave Inglesa',
+            'valor' => 30.00,
+            'qtd_estoque' => 10,
+            'qtd_minima' => 10
+        ]);
+
+        Produto::create([
+            'name' => 'Serrote Universal',
+            'valor' => 150.00,
+            'qtd_estoque' => 5,
+            'qtd_minima' => 10
         ]);
     }
 }
